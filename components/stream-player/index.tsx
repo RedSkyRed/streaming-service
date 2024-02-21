7da323b0208494305e5eb0453ca608dad3ba4e17
milfo <lmil0027@student.monash.edu>
2024-02-22 10:32:55 +1100

    Working on the bio part of the userr

--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -10,9 +10,13 @@ import { ChatToggle } from "./chat-toggle"
 import { Chat, ChatSkeleton } from "./chat"
 import { Header, HeaderSkeleton } from "./header"
 import { InfoCard } from "./info-card"
+import { AboutCard } from "./about-card"
 
 interface StreamPlayerProps{
-    user: User & { stream: Stream | null}
+    user: User & { 
+        stream: Stream | null,
+        _count: {followedBy: number}
+    }
     stream: Stream
     isFollowing: boolean
 }
@@ -59,6 +63,13 @@ export const StreamPlayer = ({user, stream, isFollowing}: StreamPlayerProps) => 
                     viewerIdentity={identity}
                     name={stream.name}
                     thumbnailUrl={stream.thumbnailUrl}
+                />
+                <AboutCard
+                    hostName={user.username}
+                    hostIdentity={user.id}
+                    viewerIdentity={identity}
+                    bio={user.bio}
+                    followedByCount={user._count.followedBy}
                 />
             </div>
             <div className={cn("col-span-1", collapsed && "hidden")}>
